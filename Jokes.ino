3920ce42e9afa4da8eb2622fdd3eb136b1da8ced
ObsidianOrder <19shaunakm@students.harker.org>
2013-08-12 09:00:53 -0700

    Create Jokes.ino

---
+++ Jokes.ino
@@ -0,0 +1,162 @@
+Overheard in a corridor:
+Crewman: "I've got a brother at Starfleet Science Academy."
+
+Crewwoman: "What's he studying?"
+
+Crewman: "Nothin'. They're studying him."
+
+The Kzinti had captured a Medusan, but since Medusans are energy beings, they had trouble deciding how to eat him. The Kzinti captain had the last word. He said they should use lots of sugar, because, "everyone knows a spoonful of sugar helps the Medusan go down."
+A young man was applying to join Starfleet:
+"Where were you born?" asked the recruiting officer.
+
+"Earth, sir."
+
+"What part?"
+
+"All of me, sir."
+
+Did you hear about the Federation weapons expert? He never forgets a phaser.
+Where does a ten-foot Mugato sleep? Anywhere he wants to.
+What do you call a ten-foot Mugato? Sir.
+When the Melkotians beamed Kirk, Spock, Chekov, and McCoy down to the recreation of the OK Corral, none of the officers knew how to use the old-style six-guns. You see, they came from a time when no man had guns before.
+Why was STAR TREK so successful? It had good Genes.
+What would you have if all the Star Trek fans in Switzerland got together?
+Show me a man who is a good loser...and I'll show you a junior officer who is playing 3-D chess with his captain.
+Captain Kirk: "Since all of you crewmembers performed so inefficiently today, there'll be no liberty at Starbase Seven."
+Voice: "Give me liberty or give me death!"
+
+Kirk: "Who said that?"
+
+Voice: "Patrick Henry."
+
+McCoy: "Should we have a friendly game of cards?"
+Kirk: "No, let's play poker."
+
+Kirk was chatting with a newly commissioned ensign when a crewman approached and asked to speak to him.
+"Go ahead, son," Kirk said. "It's kind of confidential, captain. I'd rather not say it in front of the ensign."
+
+"Well," said Kirk, "spell it then."
+
+Noticing medals on Balok's chest, Kirk asked, "Did you win those in combat?"
+"Oh, no," said Balok. "I don't believe in military service."
+
+"Did you shrink from battle?" asked Kirk.
+
+"No," shrugged Balok, "I've always been this size."
+
+When the ENTERPRISE crew beamed down to the Guardian of Forever, Dr. McCoy refused to go through.
+"You're all the same," he grumbled, "In one era and out the other."
+
+McCoy: "I've borrowed Mr. Scott's bagpipes."
+Kirk: "But you can't play them."
+
+McCoy: "While I've got them, neither can he!"
+
+Lieutenant Kyle: "Dr. McCoy, I sleep all day, stay awake all night. I'm hot all the time and can't stop dancing. And I see rings before my eyes! What's wrong with me?"
+McCoy: "Sounds like Saturn Day Night Fever."
+
+Dr. McCoy was impressed by the professional manner of new ENTERPRISE psychiatrist Dr. Zhrink. After a long shift, an amazed McCoy asked him, "how can you stay so fresh and cool after eight hours of listening to such terrible problems?"
+Dr. Zhrink shrugged. "Who listens?"
+
+McCoy: "Do you serve crabs here?"
+Mess officer: "We serve anybody. Sit down."
+
+Harry Mudd was arrested and charged with fraud for selling maps to the Fountain of Youth. When computer records were checked, it was discovered he had been arrested for the same offense in 1716, 1986, 2005, and Stardate 25.8.
+Harry Mudd was on trial again.
+"Harry," said the judge, "You're accused of throwing your wife, Stella, out of the window. This is a most serious crime."
+
+"But your honor," cried Harry, "be lenient. You've met my wife."
+
+"Yes," answered the judge with a shudder, "and I don't blame you for what you did. But don't you understand-- she could have LANDED on somebody?"
+
+Sarek and Amanda were dating
+Amanda was patiently waiting
+
+For signs of romance
+
+Soft words, a slow dance
+
+What she got was an efficiency rating
+
+What does a Romulan frog use for camouflage? A croaking device.
+It seems the Klingons had a diabolical plan to trap the ENTERPRISE in silver paper.
+Luckily, the plan was foiled.
+
+[Below are some "standard" ethnic jokes.]
+Do you know what they call a Klingon with half a brain? Gifted!
+
+Do you know what they call a Klingon with no brain at all? Normal.
+
+What is the longest four years of a Klingon's life? Third Grade.
+
+How do you get a one-armed Klingon out of a tree? Wave to him.
+
+Why can't Klingon kids play in sandboxes? Cats keep trying to cover them up.
+
+Why did the Klingon cross the road? To conquer the other side.
+
+Scotty and Sulu had been at the K-7 saloon for three hours when suddenly in walked a strange alien being. He was eight feet tall, weighted less than a hundred pounds, and had orange skin, purple hair, and six yellow eyes. To top it all off, he was wearing a red-and-blue-striped suit. Scotty stared at him for a long while and finally rose and staggered over to the being.
+"Pardon me for askin', friend, bu' wha' do ye look like when Ah'm sober?"
+
+The next day, the bartender was just opening up the place when a pink elephant and a rhinoceros came strolling in. The bartender shook his head. "Sorry, boys, Scotty hasn't come in yet."
+Dr. McCoy finished his examination of Scotty and shook his head. "Scotty, I can't find any reason for your stomach pains. Frankly, I think it's due to drinking."
+"In that case, Leonard," said Scotty, "I'll come back when you're sober."
+
+Mr. Spock: "What is the formula for PI?"
+Chekov: "Er...apple or blueberry, sir?"
+
+Mr. Spock: "A syzygy is three heavenly bodies lined up in a row. Give me an example."
+Sulu: "Mudd's Women!"
+
+A visiting admiral approached Chekov's station on the ENTERPRISE. Thinking he would test the young officer, he asked, "What would you do if the weapons officer suddenly got his head blown off?"
+"Nothing, sir."
+
+"Why nothing?"
+
+"Because I'm the weapons officer, sir."
+
+What do you call it when two science officers are having an argument?
+Science Friction.
+
+Sulu: "I've just discovered that Ilia's sister is a redhead."
+Chekov: "But I thought Deltans don't have any hair."
+
+Sulu: "She doesn't. She just has a red head."
+
+Show me Uhura reciting verse at warpspeed...and I'll show you poetry in motion.
+Uhura: "Everyone on this ship thinks I'm crazy because I like pastrami on rye."
+McCoy: "That doesn't mean you're crazy. I like pastrami on rye, too."
+
+Uhura: "Great! You must come and see my collection!"
+
+Uhura was working at her console when she suddenly straightened up. "I think there's a sick crewmember on Deck 9," she said. As no message had been received, Kirk was baffled, but sent McCoy to check it out. Sure enough, the doctor reproted that a crewmember had, indeed, collapsed where Uhura had predicted.
+Impressed, Kirk turned to her. "You must be psychic, Uhura. How did you know that crewman was ill?"
+
+Uhura smiled. "I had my ailing frequencies open, sir."
+
+And let's not forget their mission...
+"To boldly go where Nomad has gone before...."
+
+The following are lines Spock might have said followed by what was really said:
+This celebratory gathering occurs at my behest and I shall be lachrymose if it so befits me.
+
+Answer: It's my party and I'll cry if I want to.
+She chooses to purchase a terraced incline directed toward a post-life paradisiacal region.
+Answer: And she's buying a stairway to heaven.
+The leather coverings now encasing my pedal extremities have been manufactured for the specific purpose of ambulatory forward motion.
+Answer: These Boots Were Made For Walkin.
+Adieu, jaundiced vehicular pathway consisting of bricks of baked clay.
+Answer: Goodbye, Yellow Brick Road
+And we will engage in much jubilant activity until such time as the male parent chooses to repossess her vehicle of motorized transport.
+Answer: And we'll have fun, fun, fun till her daddy takes the T-bird away.
+The deity had little or nothing to do with the manufacture of minuscule viridescent seed-bearing fruits.
+Answer: God didn't make little green apples.
+Allow me the honor of portraying for you a miniaturized representation of a member of the family Ursidae of the order Carnivora.
+Answer: Let me be your Teddy Bear.
+You provide illumination for the period of time delimited by my nativity and the complete cessation of my metabolic functions.
+Answer: Again, nitpicky. It's "You Light Up My Life."
+Express deep affection towards yours truly in the manner of a hardened igneous object.
+Answer: Love me like a rock.
+Spock: I possess the capability of performing ocular scans of manual inscriptions on a vertical partition.
+Answer: I can read the writing on the wall.
+
